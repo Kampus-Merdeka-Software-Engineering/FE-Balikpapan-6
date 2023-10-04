744b6a417e796192eb2e1355caeb67ba3684168f
louisederizhy <louise.derizhy@rsmurniteguh.com>
2023-10-04 09:57:41 +0700

    implementasi ajax pada product

--- script/detailProductSpecific.js
+++ script/detailProductSpecific.js
@@ -1,32 +1,32 @@
-fetch('../json/adeAyu.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(data => {
-            generateProduct(data);
-        });
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
+async function generateProduct(category) {
+    let response = await fetch(`https://be-balikpapan-6-production.up.railway.app/api/product/getProductsByCategory/` + category);
+    let data = await response.json();
+    data = data.data;
 
-function generateProduct(data) {
+    $('#category-title').text(category);
     const productContainer = document.getElementById('productList');
-    const productDiv = document.createElement('div');
-    productDiv.classList.add('item');
+    for (let i in data) {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('item');
 
-    const image = new Image();
-    image.src = data.product_img;
-    image.alt = data.product_img;
+        const image = new Image();
+        image.src = data[i].product_img;
+        image.alt = data[i].product_img;
 
-    image.addEventListener('load', () => {
-        productDiv.innerHTML = `
-            <div class="item-image">
-                <img src="${data.product_img}" alt="${data.product_img}">
-            </div>
-            <div class="item-name" title="${data.product_name}">${data.product_name}</div>
-            <div class="item-price">IDR ${data.price}</div>
-            <div class="item-button">
-                <button onclick="window.location.href = 'detailProduct.html';">Add to Cart</button>
-            </div>
-        `;
-        productContainer.appendChild(productDiv);
-    });
+        image.addEventListener('load', () => {
+            productDiv.innerHTML = `
+                <div class="item-image">
+                    <img src="${data[i].product_img}" alt="${data[i].product_img}">
+                </div>
+                <div class="item-name" title="${data[i].product_name}">${data[i].product_name}</div>
+                <div class="item-price">IDR ${data[i].price}</div>
+                <div class="item-button">
+                    <button class="prod-button" onclick="sessionStorage.setItem('product_id', '${data[i].product_id}'); window.location.href = 'detailProduct.html';">Add to Cart</button>
+                </div>
+            `;
+            productContainer.appendChild(productDiv);
+        });
+    }
 }
+
+generateProduct(sessionStorage.getItem('category'));
